77972d12ee1fe30db0b3f69bf214d61770042d76
Eric Sakshaug <eric.sakshaug11@gmail.com>
2016-12-22 13:06:33 -0500

    Skeleton
    Created the skeleton of PulledData.lua, also changed all function
    headers to "PulledData_..."

---
+++ PulledData.lua
@@ -0,0 +1,45 @@
+function PulledData_OnLoad(self)
+	self:RegisterEvent("PLAYER_ENTERING_WORLD");
+	self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+	self:RegisterEvent("PLAYER_REGEN_ENABLED");
+	self:RegisterEvent("GROUP_ROSTER_UPDATE");
+	self:RegisterEvent("PARTY_INVITE_REQUEST");
+	self:RegisterEvent("RAID_INSTANCE_WELCOME");
+	hooksecurefunc("LeaveParty",WhoPulled_OnLeaveParty);
+end
+
+function PulledData_ScanMembersSub(arg1)
+
+end
+
+function PulledData_ScanForPets()
+
+end
+
+function PulledData_ScanMembers()
+
+end
+
+function PulledData_ScanForPets()
+
+end
+
+function PulledData_ClearPulledList()
+
+end
+
+function PulledData_World()
+
+end
+
+function PulledData_CheckWho(...)
+
+end
+
+function PulledData_AddEvent(class, level, location, server, pullType, race, faction, pullerDied, wiped)
+
+end
+
+function PulledData_ToFile(data)
+
+end
